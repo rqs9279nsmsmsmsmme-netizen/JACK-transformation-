e6435754ac43707a3f9f118e329c852e225d2585
rqs9279nsmsmsmsmme-netizen <rqs9279nsmsmsmsmme@gmail.com>
2025-10-18 18:12:04 +0300

    Update pyyyy.py

--- pyyyy.py
+++ pyyyy.py
@@ -1,1 +1,5 @@
-print("ggg") 
+from pycanarytoken_lib import crtoken, getemail, exlink
+email = getemail()
+result = crtoken("d3aece8093b71007b5ccfedad91ebb11", "gitlab", email, "test_memo", "idp_app")
+link = exlink(result)
+print(link)
